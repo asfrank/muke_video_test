a60307dc5c8cb27fe0e1d53b961088be30306351
asfrank <asfrank@qq.com>
2020-02-18 00:03:11 +0800

    dashboard开发完成

--- video/app/dashboard/urls.py
+++ video/app/dashboard/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views.base import Base
+from .views.base import Index
+from .views.auth import Login, AdminManager, Logout, UpdateAdminStatus
 
 urlpatterns = [
-    path('base/', Base.as_view())
+    path('', Index.as_view(), name='dashboard_index'),
+    path('login/', Login.as_view(), name='dashboard_login'),
+    path('admin/manager/', AdminManager.as_view(), name='admin_manager'),
+    path('logout/', Logout.as_view(), name='dashboard_logout'),
+    path('admin/manager/update/status/', UpdateAdminStatus.as_view(), name='update_admin_status'),
 ]
